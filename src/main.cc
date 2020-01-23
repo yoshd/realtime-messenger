6a8f42b79e7379ef016cc3fd4ab96d489071f1c8
yoshd <garlic.ba.0129@gmail.com>
2020-01-24 01:26:38 +0900

    WIP

--- src/main.cc
+++ src/main.cc
@@ -12,7 +12,7 @@
 #include "usecase/interactor/interactor.h"
 
 void wait(uv_idle_t* handle) {
-  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  std::this_thread::sleep_for(std::chrono::milliseconds(16));
 }
 
 int main() {
